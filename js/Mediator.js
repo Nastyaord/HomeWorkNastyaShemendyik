30da8f61160ca449fbe3db09a9152e1ad8a43aad
nastyaord <nastyaord@gmail.com>
2018-06-28 17:56:57 +0300

    Mediator pattern

--- js/Mediator.js
+++ js/Mediator.js
@@ -1,11 +1,20 @@
 class baseBlock {
+    constructor(){
+        this.students = [];
+        let allStudents = localStorage.getItem('students');
+        allStudents = JSON.parse(allStudents);
+        this.students = allStudents.allStudents;
+    }
+
+
     send(message) {
         this.mediator.send(message, this);
     }
     receive(message, from) {
-        console.log(from.block + " to " + this.block + ": " + message);
-        if (message == 'open') {
+        // console.log(from.block + " to " + this.block + ": " + message);
 
+        if (this[`${message[0]}`] !== undefined) {
+            this[`${message[0]}`](message[1]);
         }
     }
 };
@@ -14,24 +23,21 @@ class StudentsList extends baseBlock{
     constructor (block){
        super();
 
-       this.students = [];
        this.block = block;
        this.mediator;
-
-       this.getStudentsData();
+       this.sortStudents();
        this.drawStudentsList();
        this.addListeners();
     }
 
-    getStudentsData(){
-        let allStudents = localStorage.getItem('students');
-        allStudents = JSON.parse(allStudents);
+    sortStudents(){
         let students = [];
-        allStudents.allStudents.forEach(function (elem) {
+        this.students.forEach(function (elem, studentId) {
             let index = elem[4];
             if (students[index] === undefined){
                 students[index] = [];
             }
+            elem.push(studentId);
             students[index].push(elem);
         });
         this.students = students;
@@ -42,25 +48,77 @@ class StudentsList extends baseBlock{
         this.students.forEach(function (elem, index) {
             $('<ul>')
                 .text(index)
-                .appendTo(that.block);
+                .appendTo(that.block)
+                .attr('id', index);
             elem.forEach(function (elem, index) {
                 $('<li>')
                     .text(elem[0])
+                    .attr('id', elem[7])
                     .appendTo(that.block + ' ul:last-child');
             });
         });
     }
 
     addListeners(){
-        $(this.block + ' ul').on('click', this.openList.bind(this));
+        $(this.block + ' ul').on('click', this.toggleList.bind(this));
+        $(this.block + ' ul > li').on('click', this.showStudent.bind(this));
     }
 
-    openList(e){
-        $(e.target)
-            .toggleClass('open');
-        this.send('open');
+    toggleList(e){
+        let id = $(e.target)
+            .toggleClass('open')
+            .attr('id');
+        let message = ['toggleOtherList', id];
+        this.send(message);
     }
+
+    toggleOtherList(id){
+       $(this.block + ' ul[id = ' + id +']')
+           .toggleClass('open');
+    }
+
+    showStudent(e){
+        e.stopPropagation();
+        let studentId = $(e.target)
+            .attr('id');
+
+        let message = ['showStudentsData', studentId];
+        this.send(message);
+    }
+
 }
+
+
+class StudentsImages extends baseBlock{
+    constructor(block){
+        super();
+
+        this.block = block;
+    }
+
+    showStudentsData(studentId){
+        $(this.block)
+            .text(this.students[studentId]);
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 var Mediator = function() {
     var participants = {};
@@ -88,5 +146,14 @@ let mediator = new Mediator();
 let topStudentsList =  new StudentsList('#topStudentsList');
 let rightStudentsList =  new StudentsList('#rightStudentsList');
 
+let studentsImage = new StudentsImages('#studentsImage');
+
 mediator.register(topStudentsList);
 mediator.register(rightStudentsList);
+
+mediator.register(studentsImage);
+
+
+function log(a) {
+    console.log(a);
+}
